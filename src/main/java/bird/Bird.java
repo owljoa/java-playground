bf26b3bc844f09ec725d939420d661bc23d1fd82
Kim Dong In <needshield@gmail.com>
2021-11-30 20:44:37 +0900

    add override methods to serialize transient marked field (speed)

--- src/main/java/bird/Bird.java
+++ src/main/java/bird/Bird.java
@@ -1,5 +1,8 @@
 package bird;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class Bird implements Serializable {
@@ -9,6 +12,18 @@ public class Bird implements Serializable {
   private int height;
   private String type;
   transient int speed;
+
+  private void writeObject(ObjectOutputStream objectOutputStream) throws IOException {
+    objectOutputStream.defaultWriteObject();
+    objectOutputStream.writeObject(getSpeed());
+  }
+
+  private void readObject(ObjectInputStream objectInputStream)
+    throws IOException, ClassNotFoundException {
+    objectInputStream.defaultReadObject();
+    int speed = (int) objectInputStream.readObject();
+    this.setSpeed(speed);
+  }
 
   public static String getAnimalCode() {
     return animalCode;
